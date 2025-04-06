7dd09e8529aea3acf14e766eb10d5a320518b848
AboodCohen <abood.cohen@gmail.com>
2025-04-06 16:06:25 +0300

    Add files via upload

---
+++ caselog/README.md
@@ -0,0 +1,6 @@
+# CaseLog
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Abood-Cohen/pen/yyLMmaO](https://codepen.io/Abood-Cohen/pen/yyLMmaO).
+
---
+++ caselog/caselog-app-page.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+Template Name: CaseLog App Page 
+*/
+get_header(); 
+?>
+
+<div id="caselog-app">
+  <!-- This is where your HTML app content will go -->
+  <h1>CaseLog App</h1>
+  <p>Here you’ll soon see your app running directly in WordPress!</p>
+</div>
+
+<script>
+  // This is where your JavaScript will go
+  console.log('Your CaseLog app script will run from here.');
+</script>
+
+<?php get_footer(); ?>
